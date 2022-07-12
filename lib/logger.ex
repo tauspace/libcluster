afc32ea02170286ff6292d6eab9f33fd4ed03c7b
D Aguilera <109160672+DAguileraAtTauspace@users.noreply.github.com>
2022-07-12 10:03:16 -0500

    Update logger.ex

--- lib/logger.ex
+++ lib/logger.ex
@@ -11,13 +11,40 @@ defmodule Cluster.Logger do
         Logger.debug(log_message(t, msg))
     end
   end
+  
+  def info(t, msg) do
+    case Application.get_env(:libcluster, :info, false) do
+      info when info in [nil, false, "false"] ->
+        :ok
 
-  def info(t, msg), do: Logger.info(log_message(t, msg))
-  def warn(t, msg), do: Logger.warn(log_message(t, msg))
-  def error(t, msg), do: Logger.error(log_message(t, msg))
+      _ ->
+        Logger.info(log_message(t, msg))
+    end
+  end
+  
+  def warn(t, msg) do
+    case Application.get_env(:libcluster, :warn, false) do
+      warn when warn in [nil, false, "false"] ->
+        :ok
+
+      _ ->
+        Logger.warn(log_message(t, msg))
+    end
+  end
+
+  def error(t, msg) do
+    case Application.get_env(:libcluster, :error, false) do
+      e when e in [nil, false, "false"] ->
+        :ok
+
+      _ ->
+        Logger.error(log_message(t, msg))
+    end
+  end
 
   @compile {:inline, log_message: 2}
   defp log_message(t, msg) do
     "[libcluster:#{t}] #{msg}"
   end
 end
+
